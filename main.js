c79d29b47db6a771afc7999055b7073555da2382
paolo-della-fera <paolodf2005@gmail.com>
2026-02-09 15:55:43 +0100

    add palindrome function

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 // inizio chiededo la parola all'utente 
 const word = prompt('Scrivi qui la tua parola')
+
+// mi creo la funzione per controllare se la parola inserita è palindroma 
+function palindromeWord(word) {
+    let reverse_word = ''
+
+    
+}
